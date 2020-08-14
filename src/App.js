017e75de69769cbe8100a8bd97ce11403f0776c1
McKenzie Tucker <mktucker41@gmail.com>
2020-08-14 13:48:15 -0500

    welcome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import Four04 from "./components/four04/Four04";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="grading facilitator" />}
+        />
+        <Route
+          path="/welcome/:name"
+          render={(props) => (
+            <Welcome {...props} name={props.match.params.name} />
+          )}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Four04} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/four04/Four04.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Four04(props) {
+  return <div className="Four04">404</div>;
+}
+
+export default Four04;
